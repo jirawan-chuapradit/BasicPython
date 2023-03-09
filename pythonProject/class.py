66653b21628f0410d9f76a675b5b3869760143c0
Jirawan Chuapradit <jirawan-chuapradit@users.noreply.github.com>
2023-03-09 20:46:56 +0700

    create object

--- pythonProject/class.py
+++ pythonProject/class.py
@@ -1,6 +1,16 @@
+# 1 create class
+# 2 create object
+#  อธิบายว่า self เป็น ไม่ต้องส่งเข้าไป 
+# 3 ลอง print
+
+
+
 class Tank:
     def __init__(self, name, ammo):
         # self ตัวใส่ข้อมูล รถถัง และคืนค่าออกไปให้คนสั่งซื้อรถถัง
         self.name = name
         self.ammo = ammo
-        
+    
+tank_1 = Tank("zero", 3)
+
+print(tank_1.ammo)
