a265620779ebf9f8d90d986fc2611cfc127fb64a
Jirawan Chuapradit <jirawan-chuapradit@users.noreply.github.com>
2023-03-09 19:49:12 +0700

    set buuton click and show message

--- pythonProject/basic_gui.py
+++ pythonProject/basic_gui.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 
+def set_message():
+    text = text_input.get() # get message in text_input var
+    title_label.configure(text=text) # เซ็ตข้อความใน title label
 window = tk.Tk()
 window.title("basic python")
 window.minsize(width=200, height=200) # ต้องใส่ค่า default ไว้ไม่งั้นมันจะ เล็กมาก
@@ -13,8 +16,15 @@ text_input = tk.Entry(master=window)
 text_input.pack()
 
 #  button
-ok_buuton = tk.Button(master=window,text="ok")
+ok_buuton = tk.Button(master=window,text="ok", command=set_message) # 4 ใส่ชื่อ function สังเกตุว่าจะไม่ใส่ วงเล็บ เพราะว่าไม่ต้องการให้ function ทำงานก่อนจะกด
 ok_buuton.pack()
+
+# 1 เมื่อกรอกข้อความอะไรเข้าไป ให้แสดงข้อความที่กรอกเข้าไป
+# 2 บอก ปุ่มว่า ถ้ากด ให้ไปเรียกใช้งาน function
+# 3 create function
+# เมื่อปุ่มถูกกด ฟังก์ จะทำงาน ลองรัน
+
+
 window.mainloop()
 
 
