58db845b50252dff784fcdb4067d6dc5b9268d4e
Jirawan Chuapradit <jirawan-chuapradit@users.noreply.github.com>
2023-03-09 19:39:20 +0700

    insert ui text

--- pythonProject/basic_gui.py
+++ pythonProject/basic_gui.py
@@ -4,6 +4,9 @@ window = tk.Tk()
 window.title("basic python")
 window.minsize(width=200, height=200) # ต้องใส่ค่า default ไว้ไม่งั้นมันจะ เล็กมาก
 
+# ui ข้อความ 
+title_label = tk.Label(master=window, text="calculator") #บอกว่า lebel นี้จะถูกใส่ไว้ที่ไหน ในตัวอย่างนี้คือ window
+title_label.pack()# เอา label แปะ ลงไป
 
 
 window.mainloop()
